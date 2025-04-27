c01b00b1dffd2ae54a88d3b7031f1befc84a4f48
raifehizlan <raifehizlan@gmail.com>
2025-04-27 14:04:39 +0200

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Portfolyo Projesi</h1>
+      {/* Diğer bileşenler burada yer alacak */}
+    </div>
+  );
+}
+
+export default App;
